ca71e7c255aaf1505602d16358611e00bd8fbd40
Sebastian Gonzalez <sgonzalez@KVZ-SGONZALEZ.local>
2018-04-15 01:57:34 -0300

    Estilos con PostCS

---
+++ postcss-loader/webpack.config.js
@@ -0,0 +1,129 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/js/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: "./dist/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        // use: [
+        //   { loader: "style-loader" }, // Agrega el css al DOM en un <style>
+        //   { loader: "css-loader" }, // interpreta los archivos css en js via import
+        // ]
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015','react']
+          }
+        }
+      },
+      {
+        test: /\.(png|jpg|gif|woff|eot|ttf|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 100000
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(json)$/,
+        use: 'json-loader'
+      },
+      {
+        test: /\.scss$/,
+        // use: [
+        //   { loader: "style-loader" }, // Agrega el css al DOM en un <style>
+        //   { loader: "css-loader" }, // interpreta los archivos css en js via import
+        // ]
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ["css-loader","sass-loader"]
+        })
+      },
+      {
+        // test: que tipo de archivo quiero reconocer,
+        // use: que loader se va a encargar del archivo
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({
+          // ['style-loader','css-loader']
+          // fallback: 'style-loader',
+          use: ["css-loader", {
+            loader: 'less-loader',
+            options: {
+              noIeCompat: true,
+            }
+          }]
+        }),
+      },
+      {
+        // test: que tipo de archivo quiero reconocer,
+        // use: que loader se va a encargar del archivo
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          // ['style-loader','css-loader']
+          // fallback: 'style-loader',
+          use: [
+            {
+              //archivos css se manejan
+              loader: 'css-loader',
+              options: {
+                //importa archivos css dentro del css
+                modules: true,
+                //configuracion para permitir incluir otros loaders
+                importLoaders: 1
+              }
+            },
+            'postcss-loader'
+          ]
+        }),
+      }
+      ,
+      {
+        // test: que tipo de archivo quiero reconocer,
+        // use: que loader se va a encargar del archivo
+        test: /\.styl$/,
+        use: ExtractTextPlugin.extract({
+          // ['style-loader','css-loader']
+          // fallback: 'style-loader',
+          use: [
+            "css-loader",
+            {
+              loader: 'stylus-loader',
+              options: {
+                use: [
+                  require('nib'),
+                  require('rupture')
+                ],
+                import: [
+                  '~nib/lib/nib/index.styl',
+                  '~rupture/rupture/index.styl'
+                ]
+              }
+            }
+          ]
+        }),
+      }
+    ]
+  },
+  plugins: [
+    // new ExtractTextPlugin("styles.css")
+    new ExtractTextPlugin("css/[name].css")
+  ]
+}
